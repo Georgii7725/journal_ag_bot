32e664854194f57eb3f3c932db8c36f6b534cd22
n0n4m3 <67657503+n0-n4-m3@users.noreply.github.com>
2022-09-22 00:25:15 +0300

    Всё замерждил
    Добавил дату, все проверки, избавился от глобал, починил help, Убрал логин и логаут

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from turtle import home
 from imports import *
     
 async def on_startup(_):
@@ -11,10 +10,7 @@ async def get_command_start(message: types.Message):
     
     answer_start = f"Приветствую, {message.from_user.full_name}. С вами бот, который поможет тебе временно выйти из АГ! Команда 'HELP' расскажет о функционале"
 
-    await bot.send_message( chat_id=message.from_user.id,
-                            text=answer_start,
-                            parse_mode="HTML",
-                            reply_markup=kb_start)
+    await message.answer(text=answer_start,parse_mode="HTML",reply_markup=kb_start)
 
 
 # HELP COMMAND
@@ -22,15 +18,19 @@ async def get_command_start(message: types.Message):
 async def get_command_help(callback: types.CallbackQuery):
 
     help_answer = """
-    <b>REGISTER</b> - <em>регистрация ученика (при условии проживания в общежитии АГ)</em>\n<b>LOGIN</b> - <em>вход в аккаунт ученика</em>"""
+    <b>REGISTER</b> - <em>регистрация ученика (при условии проживания в общежитии АГ)</em>"""
 
-    await bot.answer_callback_query(callback.id)
+    await callback.answer_callback_query(callback.id)
     
-    await bot.send_message( chat_id=callback.from_user.id,
-                            text=help_answer,
-                            parse_mode="HTML",
-                            reply_markup=kb)
+    await callback.message.answer(text=help_answer,parse_mode="HTML",reply_markup=kb)
 
+@dp.message_handler(text=['HELP'])
+async def get_command_help(message: types.Message):
+
+    help_answer = """
+    <b>REGISTER</b> - <em>регистрация ученика (при условии проживания в общежитии АГ)</em>\n<b>LOGIN</b> - <em>вход в аккаунт ученика</em>"""
+    
+    await message.answer(text=help_answer,parse_mode="HTML",reply_markup=kb)
 
 # USER INFO COMMAND
 @dp.message_handler(text=['PROFILE'])
@@ -51,7 +51,7 @@ async def print_user_info(message: types.Message):
         """
     else:
         my_ans = "<b>Вы не вошли в систему или не зарегистрированы в ней</b>.\n <em>Для подробной информации напишите /help</em>"
-    await bot.send_message(chat_id, text=my_ans,parse_mode="HTML",)
+    await message.answer(text=my_ans,parse_mode="HTML",)
 
 
 # REGISTER COMMAND
@@ -61,10 +61,7 @@ async def add_new_user(callback: types.CallbackQuery):
     
     await bot.answer_callback_query(callback.id)
 
-    await bot.send_message( chat_id=callback.from_user.id,
-                            text=text,
-                            parse_mode="HTML",
-                            reply_markup=ReplyKeyboardRemove())
+    await callback.message.answer(text=text,parse_mode="HTML",reply_markup=ReplyKeyboardRemove())
     await register.FIO.set()
 
 # STATE FIO
@@ -77,16 +74,12 @@ async def FIO(message: types.Message, state=FSMContext):
         text="""<b>Второй шаг регистрации:</b> \n<em>Введи номер комнаты в общежитии</em>"""
 
         await state.update_data(FIO=FIO)
-        await bot.send_message( chat_id=message.from_user.id,
-                            parse_mode="HTML",
-                            text=text)
+        await message.answer(parse_mode="HTML",text=text)
         await register.num_room.set()
     except:
-        text="""<b>Даун пиши ФИО правильно</b>"""
+        text="""<b>Напишите полное ФИО</b>"""
 
-        await bot.send_message( chat_id=message.from_user.id,
-                            parse_mode="HTML",
-                            text=text)
+        await message.answer(parse_mode="HTML",text=text)
         return
 
 
@@ -99,9 +92,7 @@ async def ROOM(message: types.Message, state=FSMContext):
     text="""<b>Третий шаг регистрации:</b> \n<em>Введи st-логин для подтверждения аккаунта по почте\nСкорее всего оно окажется в спаме</em>"""
 
     await state.update_data(num_room=ans)
-    await bot.send_message( chat_id=message.from_user.id,
-                            parse_mode="HTML",
-                            text=text)
+    await message.answer(parse_mode="HTML",text=text)
     await register.st_reg.set()
 
 # STATE ST - LOGIN IN REGISTER
@@ -109,15 +100,13 @@ async def ROOM(message: types.Message, state=FSMContext):
 async def st(message: types.Message, state=FSMContext):
     
     ans = message.text  
-    global reg_active_code
+    #global reg_active_code
     reg_active_code = send_mail(ans)
 
     text="""<b>Четвёртый шаг регистрации:</b> \n<em>Введи код подтверждения</em>"""
 
-    await state.update_data(st_reg=ans)
-    await bot.send_message( chat_id=message.from_user.id,
-                            parse_mode="HTML",
-                            text=text)
+    await state.update_data(st_reg={ans,reg_active_code})
+    await message.answer(parse_mode="HTML",text=text)
     await register.check_code_reg.set()
 
 
@@ -126,137 +115,51 @@ async def st(message: types.Message, state=FSMContext):
 async def check(message: types.Message, state=FSMContext):
     await state.update_data(check_code_reg = message.text)
     data = await state.get_data()
-    if data['check_code_reg'] == reg_active_code:
-        text="""<b>Регистрация прошла успешно.</b> \n<em>Система зафиксировала, что ты являешься учеником АГ!</em>"""
-        FIO = data['FIO']
-        surname, name, lastname = FIO[0], FIO[1], FIO[2]
-        with open('users.csv', 'a') as file:
-            file.write(f"{message.from_user.id},{surname},{name},{lastname},{data['num_room']},{data['st_reg']},22:00\n")        
-
-        await bot.send_message( chat_id=message.from_user.id,
-                                text=text,
-                                parse_mode="HTML",
-                                reply_markup=kb_in)
-    else:
-        text="""Регистрация не удалась.\nВозможно вы ввели некорректный код активации"""
-        await bot.send_message( chat_id=message.from_user.id,
-                                text=text,
-                                parse_mode="HTML",
-                                reply_markup=kb_start)
-
-
+    kb = InlineKeyboardMarkup()
+    for item in list(data['st_reg']):
+        if data['check_code_reg'] == item:
+            kb = kb_in
+            text="""<b>Регистрация прошла успешно.</b> \n<em>Система зафиксировала, что ты являешься учеником АГ!</em>"""
+            FIO = data['FIO']
+            surname, name, lastname = FIO[0], FIO[1], FIO[2]
+            with open('users.csv', 'a') as file:
+                file.write(f"{message.from_user.id},{surname},{name},{lastname},{data['num_room']},{list(data['st_reg'])[0]},22:00\n")
+            break        
+        else:
+            kb = kb_start
+            text="""Регистрация не удалась.\nВозможно вы ввели некорректный код активации"""
+    await message.answer(text=text,parse_mode="HTML",reply_markup=kb)
     await state.finish()
-
-
-# LOGIN COMMAND
-@dp.callback_query_handler(text='LOGIN')
-async def get_command_login(callback: types.CallbackQuery):
-
-    text="""<em>Введи st-логин для подтверждения по почте</em>"""
-
-    await bot.answer_callback_query(callback.id)
-
-    await bot.send_message( chat_id=callback.from_user.id,
-                            text=text,
-                            parse_mode="HTML",
-                            reply_markup=ReplyKeyboardRemove())
-    await login.st_log.set()
-
-
-# STATE ST - LOGIN IN LOGIN
-@dp.message_handler(state=login.st_log)
-async def state_log(message: types.Message, state=FSMContext):
-    ans = message.text
-    global index #Нужен global, чтобы в следующем handler обратиться
-    flag, index = check_reg(ans)
-    if flag:
-        global log_active_code
-        log_active_code = send_mail(ans)
-        await state.update_data(st_log=ans)
-        text="""<em>Введи код подтверждения по st-почте</em>"""
-        await bot.send_message( chat_id=message.from_user.id,
-                            parse_mode="HTML",
-                            text=text)
-    else:
-        text="""<em>Пользователь с таким st не зарегистрирован в нашей системе</em>"""
-        await bot.send_message( chat_id=message.from_user.id,
-                            parse_mode="HTML",
-                            text=text)
-        await get_command_start(message)
-
-    await login.check_code_login.set()
-
-
-# STATE CHECK E-MAIL CODE IN LOGIN
-@dp.message_handler(state=login.check_code_login)
-async def check_code_login_def(message: types.Message, state=FSMContext):
-    ans = message.text
-    if log_active_code == ans:
-        data = await state.get_data()
-        fl, index = check_reg(data['st_log'])
-        with open('users.csv', 'r') as fileREAD:
-            data = fileREAD.readlines()
-            user = data[index].split(',')
-            user[0] = str(message.from_user.id)
-            update_user = ""
-            for charact in user:
-                update_user += charact + ","
-            update_user = update_user[:-1]
-            data[index] = update_user
-            users = ""
-            for user in data:
-                users += user
-        with open('users.csv', 'w') as fileWRITE:
-            fileWRITE.write(users)
-
-        text="""<b>Авторизация прошла успешно.</b> \n<b>Добро пожаловать!</b>"""
-    else:
-        text="""<b>Вы не смогли войти в аккаунт.</b> \n<b>Скорее всего вы указали неправильный код активации</b>"""
-
-    await bot.send_message( chat_id=message.from_user.id,
-                                text=text,
-                                parse_mode="HTML",
-                                reply_markup=kb_in)
-
-    await state.finish()
-
-
-# LOGOUT COMMAND
-@dp.message_handler(text='LOGOUT')
-async def get_command_login(message: types.Message):
-
-    flag, index = check_log(str(message.from_user.id))
-    if flag:
-        with open('users.csv', 'r') as fileREAD:
-            data = fileREAD.readlines()
-            update_user = data[index].replace(str(message.from_user.id), '-1')
-            data[index] = update_user
-            update_data = ""
-            for user in data:
-                update_data += user
-        with  open('users.csv', 'w') as fileWRITE:
-            fileWRITE.write(update_data)
-
-        text="<b>Вы успешно вышли из аккаунта!</b>"
-    else:
-        text="<b>Вы не в системе!</b>"
-
-    await bot.send_message( chat_id=message.from_user.id,
-                            text=text,
-                            parse_mode="HTML",
-                            reply_markup=kb_start)
 
 # EXIT COMMAND
-@dp.message_handler(text=['EXIT'])
+@dp.message_handler(text='EXIT')
+async def exit_calendar(message: types.Message):
+    await message.answer(text="""<b>Выберите дату прихода в АГ</b>""",
+                         parse_mode="HTML",
+                         reply_markup=ReplyKeyboardRemove())
+    await message.answer(text="""<em>Если вы вернетесь этим же днём, \nвыберите сегодняшнюю дату:</em>""",
+                         parse_mode="HTML",
+                         reply_markup=await DialogCalendar().start_calendar())
+
+# CALENDAR
+@dp.callback_query_handler(dialog_cal_callback.filter())
+async def process_dialog_calendar(callback_query: types.CallbackQuery, callback_data: dict, state: FSMContext):
+    selected, date = await DialogCalendar().process_selection(callback_query, callback_data)
+    if selected:
+        await bot.answer_callback_query(callback_query.id)
+        await exit.cal_date.set()
+        await state.update_data(cal_date=date.strftime("%d.%m.%Y"))
+        await callback_query.message.answer(f"""<b>Ты выбрал: {date.strftime("%d.%m.%Y")}</b>""", parse_mode="HTML")
+        
+        await add_new_user(callback_query.message)
+
+# STATE cal_date
+@dp.message_handler(state=exit.cal_date)
 async def add_new_user(message: types.Message):
     text="""<b>Укажите время возвращения</b> \n<em>Формат: ЧЧ:ММ</em>"""
 
-    await bot.send_message( chat_id=message.from_user.id,
-                            text=text,
-                            parse_mode="HTML",
-                            reply_markup=ReplyKeyboardRemove())
+    await message.answer(text=text,parse_mode="HTML",reply_markup=ReplyKeyboardRemove())
     await exit.entrance_time.set()
-
 
 # STATE get_entrance_time
 @dp.message_handler(state=exit.entrance_time)
@@ -279,9 +182,7 @@ async def state1(message: types.Message, state=FSMContext):
                 text="""<b>Укажите причину выхода</b>"""
                 await state.update_data(entrance_time = f'{hour_entrance}:{min_entrance}')
                 #await state.update_data(exit_time=f'{hour_exit}:{min_exit}')
-                await bot.send_message( chat_id=message.from_user.id,
-                                parse_mode="HTML",
-                                text=text)
+                await message.answer(parse_mode="HTML",text=text)
                 await exit.reason.set()
             else:
                 await message.answer("Вам не разрешено приходить в это время", reply_markup=kb_in)
@@ -314,11 +215,7 @@ async def state3(message: types.Message, state=FSMContext):
     with open('database.csv', 'a') as file: 
         file.write(f"{message.from_user.id},{f'{hour_exit}:{min_exit}'},{data['entrance_time']},{data['reason']},False\n")
 
-    await bot.send_message( chat_id=message.from_user.id,
-                            parse_mode="HTML",
-                            text=text,
-                            reply_markup=kb_out
-                            )
+    await message.answer(parse_mode="HTML",text=text,reply_markup=kb_out)
     await exit.flag.set()
 
 # STATE flag
@@ -336,13 +233,17 @@ async def state4(message: types.Message, state=FSMContext):
     with  open('database.csv', 'w') as fileWRITE:
         fileWRITE.write(update_data)
 
-
-    await bot.send_message( chat_id=message.from_user.id,
-                            text=text,
-                            parse_mode="HTML",
-                            reply_markup=kb_in)
+    await message.answer(text=text,parse_mode="HTML",reply_markup=kb_in)
 
     await state.finish()
+
+# ROUND CONNECT FROM USER
+@dp.message_handler(text='COMMENT')
+async def round_connect_from_users(message: types.Message):
+    text="""<b>Обратная связь</b>\n<em>Если у вас есть пожелания, просьбы для улучшения нашего бота</em>\n<em>или вы нашли ошибку в работе бота, то оставте отзыв в Google Forms.</em>\n<em>Этот комментарий останеться </em><b>АНОНИМНЫМ:</b>\n<a href="https://forms.gle/ovk73RWEPuCqbCd36">Google Forms</a>"""
+
+    await message.answer(text=text,
+                         parse_mode="HTML")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
