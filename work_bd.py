060e523fd50369bbb70e4743ba5f5c015e088627
Georgii7725 <georgiibiryukov@gmail.com>
2023-02-13 23:11:40 +0300

    work_bd

--- work_bd.py
+++ work_bd.py
@@ -1,21 +1,17 @@
 import sqlite3
-from sqlite3 import Error
+from sqlite3 import Connection, Error
 import traceback
 
 
 # Создание БД
-def create_connection(path):
+def create_connection(path) -> Connection:
     connection = None
     try:
         connection = sqlite3.connect(path)
     except Error as e:
         print(f"The error '{e}' occurred")
-
     return connection
 
-
-
-# DML команды
 def add_user(user: list):
     connection = create_connection("journal_ag_bot.sqlite")
     cursor = connection.cursor()
@@ -24,26 +20,25 @@ def add_user(user: list):
         cursor.execute(query, user)
         connection.commit()
     except Error as e:
-        print(f"The error '{e}' occurred.\n{query}\n{traceback.format_exc()}")
+        print(traceback.format_exc())
     connection.close()
 
-def get_user(tg_id):
+def get_user(tg_id: int) -> list:
     connection = create_connection("journal_ag_bot.sqlite")
     cursor = connection.cursor()
-    query = f"SELECT * FROM users WHERE tg_id = {tg_id}"
+    query = "SELECT * FROM users WHERE tg_id = ?"
     try:
-        cursor.execute("SELECT * FROM users WHERE tg_id = ?", [tg_id])
+        cursor.execute(query, [tg_id])
         user = cursor.fetchone()
         connection.commit()
         connection.close()
         return user
     except Error as e:
-        print(f"The error '{e}' occurred.\n{query}\n{traceback.format_exc()}")
+        print(traceback.format_exc())
         connection.close
-        return None
+        return [None] * 7
 
-
-def get_st(tg_id):
+def get_st(tg_id: int) -> str:
     connection = create_connection("journal_ag_bot.sqlite")
     cursor = connection.cursor()
     query = "SELECT st FROM users WHERE tg_id = ?"
@@ -54,24 +49,22 @@ def get_st(tg_id):
         connection.close()
         return st[0]
     except Error as e:
-        print(f"The error '{e}' occurred.\n{query}\n{traceback.format_exc()}")
+        print(traceback.format_exc())
         connection.close()
         return None
-
 
 def add_exit(params: list):
     connection = create_connection("journal_ag_bot.sqlite")
     cursor = connection.cursor()
     query = "INSERT INTO database (KEY, exit_date, exit_time, entrance_date, entrance_time, reason, flag) VALUES (?, ?, ?, ?, ?, ?, 0);"
-
     try:
         cursor.execute(query, params)
         connection.commit()
     except Error as e:
-        print(f"The error '{e}' occurred.\n{query}\n{traceback.format_exc()}")
+        print(traceback.format_exc())
     connection.close()
 
-def update_database(st):
+def update_database(st: str):
     connection = create_connection("journal_ag_bot.sqlite")
     cursor = connection.cursor()
     query = "UPDATE database SET flag = True WHERE KEY = ?"
@@ -79,25 +72,26 @@ def update_database(st):
         cursor.execute(query, [st])
         connection.commit()
     except Error as e:
-        print(f"The error '{e}' occurred.\n{query}\n{traceback.format_exc()}")
+        print(traceback.format_exc())
     connection.close()
 
-def get_al_time_room_from_adm(surname, name):
+def get_al_time_room_from_adm(surname: str, name: str) -> list:
     connection = create_connection("journal_ag_bot.sqlite")
     cursor = connection.cursor()
     query = "SELECT al_time, room FROM administration WHERE surname = ? AND name = ?"
     try:
+        print
         cursor.execute(query, [surname, name])
         al_time, room = cursor.fetchone()
         connection.commit()
         connection.close()
         return al_time, room
     except Error as e:
-        print(f"The error '{e}' occurred.\n{traceback.format_exc()}")
+        print(traceback.format_exc())
         connection.close()
         return None
 
-def get_al_time_from_users(tg_id):
+def get_al_time_from_users(tg_id: int) -> str:
     connection = create_connection("journal_ag_bot.sqlite")
     cursor = connection.cursor()
     query = "SELECT al_time FROM users WHERE tg_id = ?"
@@ -106,14 +100,13 @@ def get_al_time_from_users(tg_id):
         al_time = cursor.fetchone()
         connection.commit()
         connection.close()
-        return al_time
+        return al_time[0]
     except Error as e:
-        print(f"The error '{e}' occurred.\n{query}\n{traceback.format_exc()}")
+        print(traceback.format_exc())
         connection.close()
         return None
 
-
-def check_exists_tg_id(tg_id):    # ["surname", "name"]
+def check_exists_tg_id(tg_id: int) -> bool:   
     connection = create_connection("journal_ag_bot.sqlite")
     cursor = connection.cursor()
     query = "SELECT EXISTS(SELECT * FROM users WHERE tg_id = ?)"
@@ -124,12 +117,11 @@ def check_exists_tg_id(tg_id):    # ["surname", "name"]
         connection.close()
         return flag[0]
     except Error as e:
-        print(f"The error '{e}' occurred.\n{query}\n{traceback.format_exc()}")
+        print(traceback.format_exc())
         connection.close()
         return False
 
-
-def check_is_user_out(st):
+def check_is_user_out(st: str) -> bool:
     connection = create_connection("journal_ag_bot.sqlite")
     cursor = connection.cursor()
     query = "SELECT flag FROM database WHERE KEY = ?"
@@ -140,27 +132,26 @@ def check_is_user_out(st):
         connection.close()
         return flag[-1][0]
     except Error as e:
-        print(f"The error '{e}' occurred.\n{query}\n{traceback.format_exc()}")
+        print(traceback.format_exc())
         connection.close()
         return False
 
-
-def check_exists_FI(surname, name):
+def check_exists_FI(surname: str, name: str) -> bool:
     connection = create_connection("journal_ag_bot.sqlite")
     cursor = connection.cursor()
     query = "SELECT EXISTS(SELECT * FROM administration WHERE surname = ? and name = ?)"
     try:
-        flag = cursor.execute(query, [surname, name])
-        flag = flag.fetchone() # ты возвращал объект cursor. if в регистрациии нихуя не понимал что имел ввиду под cursor
+        cursor.execute(query, [surname, name])
+        flag = cursor.fetchone()
         connection.commit()
         connection.close()
-        return bool(flag[0])# инедкс 0, тк это кортеж tuple и когда ты просто писал flag там был вывод (0,) а bool просто мне нравится bool
+        return flag[0]
     except Error as e:
-        print(f"The error '{e}' occurred.\n{query}\n{traceback.format_exc()}")
+        print(traceback.format_exc())
         connection.close()
-        return False #Гош да ты гений я тут блять сижду переделываю последний хендлер чтобы выдавало что такого имени и фамилии нет а тыт тут true возвращал
+        return False
 
-def check_exists_st(st: str):
+def check_exists_st(st: str) -> bool:
     connection = create_connection("journal_ag_bot.sqlite")
     cursor = connection.cursor()
     query = "SELECT EXISTS(SELECT * FROM users WHERE st = ?)"
@@ -171,7 +162,7 @@ def check_exists_st(st: str):
         connection.close()
         return flag[0]
     except Error as e:
-        print(f"The error '{e}' occurred.\n{query}\n{traceback.format_exc()}")
+        print(traceback.format_exc())
         connection.close()
         return False
 
@@ -187,5 +178,4 @@ def print_table(table: str) -> list:
         return result
     except Error as e:
         connection.close()
-        print(f"The error '{e}' occurred")
-
+        print(traceback.format_exc())
